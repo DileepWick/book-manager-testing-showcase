13f41c278621767be5935ab8187b891b1ed696d5
TharinduAri <tharindulakshita2001@gmail.com>
2025-05-02 18:59:07 +0530

    Added .env.test file

--- server/config/db.js
+++ server/config/db.js
@@ -14,6 +14,21 @@ export const connectDB = async () => {
   }
 };
 
+
+export const connectTestDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    if (process.env.NODE_ENV !== 'test') {
+      console.log(`Connected to Development MongoDB 😎`);
+    }else{
+      console.log(`Connected to Test MongoDB 😎`);
+    }
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
 export const disconnectDB = async () => {
   try {
     await mongoose.disconnect();
