45c2644c0aecb3ef2be5fab004801b909b1e6250
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-05-02 12:31:36 +0530

    configured testings

--- server/server.js
+++ server/server.js
@@ -1,18 +1,41 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import bookRoutes from './routes/bookRoutes.js';
 
-dotenv.config(); // Load environment variables
+// Load environment variables
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: '.env.test' });
+} else {
+  dotenv.config(); // Load default .env
+}
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
 
-// Connect to database
-connectDB();
+// MongoDB connection
+connectDB(process.env.MONGO_URI);
+
 
 // Routes
 app.use('/api', bookRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running : http://localhost:${PORT} 😊`));
+// Start the server
+const server = app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(`Connected to Development Server: http://localhost:${port} 😊`);
+  }else{
+    console.log(`Connected to Test Server: http://localhost:${port} 😊`);
+  }
+});
+
+// Export the server for tests
+export const closeServer = () => {
+  server.close();
+};
+
+export default app;
